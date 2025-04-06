3e16fafe1bde1ed52e157f5980e4055361be81f6
Hyun Park <hyunp.dev@gmail.com>
2025-04-06 21:03:48 +0900

    upload

--- solved.ac/sprout/10807.py
+++ solved.ac/sprout/10807.py
@@ -1,8 +1,4 @@
-let _ = 
-  let n = read_int();
-  let a: string list = read_line()
-  |> String.split_on_char ' '
-  |> List.map Stdlib.int_of_string;
-  let v: int = read_int();
-  a |> List.filter (fun e: int -> e = v) |> List.;
-
+n = int(input())
+a = list(map(int, input().split()))
+v = int(input())
+print(a.count(v))
